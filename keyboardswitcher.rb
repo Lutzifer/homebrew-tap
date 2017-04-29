4ed6215ce9561ab5587ce89d76f2edcc80dc1c28
Wolfgang Lutz <WLBORg@gmx.de>
2017-04-29 18:50:10 +0200

    Update keyboardswitcher.rb

--- keyboardswitcher.rb
+++ keyboardswitcher.rb
@@ -1,13 +1,9 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/homebrew/master/frames
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Keyboardswitcher < Formula
   desc ""
   homepage ""
-  url "https://github.com/Lutzifer/keyboardSwitcher/archive/0.0.5.tar.gz"
-  version "0.0.5"
-  sha256 "cd116f23214d49fc36b34c11e741bc4567c752abda96eae07bc896ce88757762"
+  url "https://github.com/Lutzifer/keyboardSwitcher/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "4e3199a0ca738f18577411a135940db8bff5db25e9a5228c652f988b5d4177a0"
 
   def install
    xcodebuild
@@ -15,15 +11,6 @@ class Keyboardswitcher < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test keyboardSwitcher`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "keyboardSwitcher"
   end
 end
