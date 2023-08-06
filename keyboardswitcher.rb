11641437cb83621fc0be106f1c8a756cd143ebac
Wolfgang Lutz <wlut@num42.de>
2023-08-06 20:43:45 +0200

    Update keyboardswitcher.rb

--- keyboardswitcher.rb
+++ keyboardswitcher.rb
@@ -3,7 +3,7 @@ class Keyboardswitcher < Formula
   homepage "https://github.com/Lutzifer/keyboardSwitcher"
   url "https://github.com/Lutzifer/keyboardSwitcher/archive/1.0.3.tar.gz"
   version "1.0.3"
-  sha256 "e0d005105fd3972f6d3f4a0a4c71dc0112d4f8168b06503c98620653b5a4aa72"
+  sha256 "8e7409e963643f60054e7282d1f71bf7efbe270bb37c075973c4d4110666e2ec"
 
   def install
     xcodebuild
