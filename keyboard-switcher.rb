11bb803f1d96bd2908b287356d0fbcefd6c59783
Wolfgang Lutz <WLBORg@gmx.de>
2023-12-08 19:36:21 +0100

    Release KeyboardSwitcher as Binary

---
+++ keyboard-switcher.rb
@@ -0,0 +1,15 @@
+class KeyboardSwitcher < Formula
+  desc "Tool that allows to switch Keyboard layouts from the command-line"
+  homepage "https://github.com/Lutzifer/keyboardSwitcher"
+  url "https://github.com/Lutzifer/keyboardSwitcher/releases/download/v2.0.0/keyboardSwitcher"
+  version "2.0.0"
+  sha256 "ff2ea7ba102a0bdf46375a94c75ea223a1712288a5a2b72188661ba15246f58c"
+
+  def install
+    bin.install("keyboardSwitcher")
+  end
+
+  test do
+    system "keyboardSwitcher"
+  end
+end
