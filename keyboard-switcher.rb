13c9f41cc5006a072360bf887086afc2e8a2f0f8
Wolfgang Lutz <wlut@num42.de>
2025-03-23 14:33:42 +0100

    Update keyboard-switcher.rb

--- keyboard-switcher.rb
+++ keyboard-switcher.rb
@@ -1,8 +1,8 @@
 class KeyboardSwitcher < Formula
   desc "Tool that allows to switch Keyboard layouts from the command-line"
   homepage "https://github.com/Lutzifer/keyboardSwitcher"
-  url "https://github.com/Lutzifer/keyboardSwitcher/releases/download/v2.1.0/keyboardSwitcher"
-  version "2.1.0"
+  url "https://github.com/Lutzifer/keyboardSwitcher/releases/download/v2.2.0/keyboardSwitcher"
+  version "2.2.0"
   sha256 "91309d1f2dbbce61fb919e925e9f426efde694046645ccc751993f5a839a97fa"
 
   def install
