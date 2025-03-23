79d5542b149e233ca9e4fb8be9bdcd53b5b49397
Wolfgang Lutz <wlut@num42.de>
2025-03-23 14:40:43 +0100

    Update keyboardswitcher.rb

--- keyboardswitcher.rb
+++ keyboardswitcher.rb
@@ -3,7 +3,7 @@ class Keyboardswitcher < Formula
   homepage "https://github.com/Lutzifer/keyboardSwitcher"
   url "https://github.com/Lutzifer/keyboardSwitcher/releases/download/v2.2.0/keyboardSwitcher"
   version "2.2.0"
-  sha256 "91309d1f2dbbce61fb919e925e9f426efde694046645ccc751993f5a839a97fa"
+  sha256 "400b24145592a4671729c76fca7f8caf0d058ac6817e215ab79d814493d2a577"
 
   def install
     bin.install("keyboardSwitcher")
